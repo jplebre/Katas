b30f8b06f76a4c136fd85f9ceba895070188d5b9
jplebre <jplebre@gmail.com>
2015-12-09 23:26:49 +0000

    FizzBuzz: Added first step

---
+++ src/Katas.FizzBuzz/FizzBuzzTests/FizzBuzzTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace FizzBuzzTests.cs
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void FirstNumberIs()
+        {
+            Assert.That(new FizzBuzzGenerator().GetNumberAtPosition(0), Is.EqualTo(1));
+        }
+    }
+
+    public class FizzBuzzGenerator
+    {
+        public int GetNumberAtPosition(int position)
+        {
+            return 1;
+        }
+    }
+}
