ffc8f4bdf9c3588e1ad4c8ceddd1f8edd318aea9
jplebre <jplebre@gmail.com>
2015-12-10 08:01:52 +0000

    refactoring pass

--- src/Katas.FizzBuzz/FizzBuzzGenerator/FizzBuzzGenerator.cs
+++ src/Katas.FizzBuzz/FizzBuzzGenerator/FizzBuzzGenerator.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace FizzBuzzGenerator
+namespace FizzBuzzTests
 {
     public class FizzBuzzGenerator
     {
+        public int GetNumberAtPosition(int position)
+        {
+            return position;
+        }
     }
 }
--- src/Katas.FizzBuzz/FizzBuzzTests/FizzBuzzTests.cs
+++ src/Katas.FizzBuzz/FizzBuzzTests/FizzBuzzTests.cs
@@ -2,7 +2,7 @@
 using NUnit.Framework;
 using NUnit.Framework.Constraints;
 
-namespace FizzBuzzTests.cs
+namespace FizzBuzzTests
 {
     [TestFixture]
     public class FizzBuzzTests
@@ -10,21 +10,21 @@ namespace FizzBuzzTests.cs
         [Test]
         public void FirstNumberIs1()
         {
-            Assert.That(new FizzBuzzGenerator().GetNumberAtPosition(0), Is.EqualTo(1));
+            Assert.That(GetFizzBuzzResultAtPosition(1), Is.EqualTo(1));
         }
 
         [Test]
         public void SecondNumberIs2()
         {
-            Assert.That(new FizzBuzzGenerator().GetNumberAtPosition(1), Is.EqualTo(2));
+            Assert.That(GetFizzBuzzResultAtPosition(2), Is.EqualTo(2));
         }
-    }
 
-    public class FizzBuzzGenerator
-    {
-        public int GetNumberAtPosition(int position)
+
+        //----- Helper Methods -----
+        private static int GetFizzBuzzResultAtPosition(int position)
         {
-            return position + 1;
+            return new FizzBuzzGenerator().GetNumberAtPosition(position);
         }
+
     }
 }
