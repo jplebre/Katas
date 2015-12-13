fd42df245911154d0ec6f047cba973dcac46e332
jplebre <jplebre@gmail.com>
2015-12-13 01:00:19 +0000

    started Fibonacci Kata. See first test failing

---
+++ src/Katas.Fibonacci/Katas.Fibonacci.UnitTests/FibonacciTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Katas.Fibonacci.UnitTests
+{
+    [TestFixture]
+    public class FibonacciTests
+    {
+        [Test]
+        public void FibonacciNumberAtPositionOneIsZero()
+        {
+            Assert.That(new Fibonacci().FibonacciNumberAtPosition(1), Is.EqualTo(0));
+        }
+    }
+}
---
+++ src/Katas.Fibonacci/Katas.Fibonacci/Fibonacci.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas.Fibonacci
+{
+    public class Fibonacci
+    {
+        public int FibonacciNumberAtPosition(int position)
+        {
+            return -1;
+        }
+    }
+}
