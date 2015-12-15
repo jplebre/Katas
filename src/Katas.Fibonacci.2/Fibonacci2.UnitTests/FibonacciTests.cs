8fe73e4810162aa691e4e265d24a2c0f795e3317
jplebre <jplebre@gmail.com>
2015-12-15 23:50:01 +0000

    Fibonacci2 added, slightly different starting point to start TDD

---
+++ src/Katas.Fibonacci.2/Fibonacci2.UnitTests/FibonacciTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Fibonacci2.UnitTests
+{
+    [TestFixture]
+    public class FibonacciTests
+    {
+        [Test]
+        public void FibonacciGeneratorCreatesSequenceOfRightLength()
+        {
+            Assert.That(new Fibonacci().SequenceGeneratorOfLength(100), Has.Length.EqualTo(100));
+        }
+
+        [Test]
+        public void FibonacciGeneratorFirstNumberInSequenceIsZero()
+        {
+            Assert.That(GetFibonacciNumberAtPosition(1), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void FibonacciGeneratorSecondNumberInSequenceIsOne()
+        {
+            Assert.That(GetFibonacciNumberAtPosition(2), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void FibonacciGeneratorThirdNumberInSequenceIsOne()
+        {
+            Assert.That(GetFibonacciNumberAtPosition(3), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void FibonacciGeneratorFourthNumberInSequenceIsTwo()
+        {
+            Assert.That(GetFibonacciNumberAtPosition(4), Is.EqualTo(2));
+        }
+
+        //Helper Method
+        private int GetFibonacciNumberAtPosition(int position)
+        {
+            return new Fibonacci().SequenceGeneratorOfLength(position)[position - 1];
+        }
+    }
+}
---
+++ src/Katas.Fibonacci.2/Fibonacci2/Fibonacci.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci2
+{
+    public class Fibonacci
+    {
+        public int[] SequenceGeneratorOfLength(int length)
+        {
+            int[] sequence = new int[length];
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                if (i < 2) sequence[i] = i;
+                else sequence[i] = sequence[i - 1] + sequence[i - 2];
+
+            }
+            return sequence;
+        }
+    }
+}
